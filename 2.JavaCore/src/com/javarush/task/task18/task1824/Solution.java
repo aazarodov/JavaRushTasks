421fb6a2a828dbb391b1cde4f4e91a10057617db
aazarodov <aazarodov@gmail.com>
2017-03-01 09:57:52 +0300

    2017.03.01

--- 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -4,7 +4,20 @@ package com.javarush.task.task18.task1824;
 Файлы и исключения
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = "";
+        try {
+            while (true) {
+                FileInputStream file = new FileInputStream((fileName = reader.readLine()));
+                file.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(fileName);
+        }
+        reader.close();
     }
 }
